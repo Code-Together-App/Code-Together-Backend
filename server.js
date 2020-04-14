1b700d6eb3cb005842fd6843845155fda283f03d
zhao ziyi <zhaoziy@oregonstate.edu>
2020-04-14 13:30:24 -0700

    ready to deploy

--- server.js
+++ server.js
@@ -5,10 +5,11 @@ const morgan = require('morgan');
 const cors = require('cors');
 const fileupload = require('express-fileupload');
 const cookieParser = require('cookie-parser');
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
+const xss = require('xss-clean');
 const errorHandler = require('./middleware/error');
-const connectDB = require('./config/db')
-
-
+const connectDB = require('./config/db');
 
 //Load config
 dotenv.config({path: './config/config.env'});
@@ -24,7 +25,7 @@ const app = express();
 
 app.use(cors());
 
-//BOdy parser
+//Body parser
 app.use(express.json());
 
 //Cookie parser
@@ -38,6 +39,15 @@ if(process.env.NODE_ENV === 'development'){
 //File uploading
 app.use(fileupload());
 
+//Sanitize data
+app.use(mongoSanitize());
+
+//Set security headers
+app.use(helmet());
+
+//Prevent XSS attacks
+app.use(xss());
+
 //Set static folder
 app.use(express.static(path.join(__dirname,'public')));
 
@@ -46,13 +56,6 @@ app.use('/api/v1/tickets', tickets);
 app.use('/api/v1/auth',auth)
 
 app.use(errorHandler);
-
-
-
-
-
-
-
 
 
 
